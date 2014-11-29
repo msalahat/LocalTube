4f2a15f1de9dce8eae4d30ef477281c1d1f55890
Mutaz Salahat <mutaz.startappz.com>
2014-11-29 11:24:03 +0200

    init commit

---
+++ package.js
@@ -0,0 +1,12 @@
+{
+        "main": "index.html",
+        "name": "localtube",
+        "description": "NodeWebkit App that enables you to donwload youtube videos localy as mp4 files",
+        "window": {
+                "toolbar": true,
+                "title": "First nodewebkit app"
+        },
+        "dependencies": {
+                "youtube-dl": "*"
+        }
+}
